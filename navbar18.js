0ccd909d3e6c78a36e59b3cdaacf3264d7900c00
nihalraza369 <nihalraza369@gmail.com>
2025-05-02 19:33:56 +0500

    best project html,css ,js

---
+++ navbar18.js
@@ -0,0 +1,7 @@
+function toggleNav11() {
+    document.getElementById("sidebar11").classList.toggle("show");
+  }
+  function closeNav11() {
+    document.getElementById("sidebar11").classList.remove("show");
+  }
+  
